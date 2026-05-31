e6cb06527a1cba44a1d31b9e9c061919df7e36fc
abdullas2004-collab <abdullas2004@gmail.com>
2026-05-31 15:32:19 +0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ProfitGuardAI from './ProfitGuardAI.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProfitGuardAI />
+  </React.StrictMode>
+)
