61d570f7b06065a468906d30ac4474ab8339a07c
abdullas2004-collab <abdullas2004@gmail.com>
2026-06-14 14:02:16 +0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ProfitGuardAI from './ProfitGuardAI.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ProfitGuardAI />
-  </React.StrictMode>
-)
